dcaa14d7c0916913eb2f3c142ee1ae9828ae2621
Nikolay Ermakov <nk.er.ft@gmail.com>
2024-03-04 00:31:10 +0100

    Lesson4:Task1:Functions for Matrix created

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,18 +1,29 @@
-﻿int[,] matrix = new int[4, 5];
-Random rnd = new Random();
-
-for (int i = 0; i < matrix.GetLength(0); i++)
+﻿int[,] CreateMatrix(int rowCount, int columnCount)
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    int[,] matrix = new int[rowCount, columnCount];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        matrix[i, j] = rnd.Next(1, 11);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 50);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
     }
 }
 
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        Console.Write($"{matrix[i, j]} ");
-    }
-}
+int[,] matrix = CreateMatrix(10,5);
+ShowMatrix(matrix);
