034d5ffb803f965cce25559fb0106fa9d71c97cd
Nikolay Ermakov <nk.er.ft@gmail.com>
2024-03-04 00:24:56 +0100

    Lesson4:Task1:First tasks for matrixes

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int[,] matrix = new int[4, 5];
+Random rnd = new Random();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next(1, 11);
+    }
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+}
